15632a2dea271ea99adabe53eb7c6b2be6e82f35
Yesenia <yeseniasescarcega@gmail.com>
2017-03-27 17:31:32 -0600

    Se agregan archivos del ejercicio del reproductor, faltan comentarios y agregar barra de estado de la reproducción

---
+++ reproductor/reproductor.js
@@ -0,0 +1,33 @@
+console.log("Hola!");
+var pista = document.getElementById("pista");
+
+function parar() {
+  pista.load();
+}
+
+function reproducir() {
+  console.log("reproduciendo");
+  document.getElementById("pista").play();
+}
+
+function pausa(){
+  pista.pause();
+}
+
+function retroceder(){
+  pista.currentTime -= 10;
+}
+
+function avanzar(){
+  pista.currentTime += 10;
+}
+
+function vol(id){
+  console.log(id);
+  if(id == "bajar"){
+    pista.volume -= 0.1;
+  }
+  else {
+    pista.volume += 0.1;
+  }
+}
