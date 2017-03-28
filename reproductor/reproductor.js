bc30acab1139be076d6fcdc84e2679a358515c50
Yesenia <yeseniasescarcega@gmail.com>
2017-03-28 11:01:11 -0600

    Se mejoró el diseño y funcionalidad (falta comentar código)

--- reproductor/reproductor.js
+++ reproductor/reproductor.js
@@ -1,14 +1,21 @@
 console.log("Hola!");
 var pista = document.getElementById("pista");
-
+var progresoBarra = document.getElementById("progreso");
+var segundos = document.getElementById("segundos");
+// var duracionTotal =  pista.duration;
+// console.log(duracionTotal);
+segundos.innerHTML = pista.currentTime + " / " + pista.duration;
 function parar() {
   pista.load();
+
+  segundos.innerHTML = pista.currentTime + " / " + duracionTotal;
 }
 
 function reproducir() {
-  console.log("reproduciendo");
+  // console.log("reproduciendo");
   document.getElementById("pista").play();
-  console.log(pista.currentTime);
+  progresoBarra.max = pista.duration;
+  // console.log(pista.currentTime);
 }
 
 function pausa(){
@@ -24,11 +31,18 @@ function avanzar(){
 }
 
 function vol(id){
-  console.log(id);
+  // console.log(id);
   if(id == "bajar"){
     pista.volume -= 0.1;
   }
   else {
     pista.volume += 0.1;
   }
+}
+
+function mostrarProgreso(){
+  // console.log(pista.currentTime);
+  // console.log(pista.duration);
+  progresoBarra.value = pista.currentTime;
+  segundos.innerHTML = pista.currentTime + " / " + pista.duration;
 }
