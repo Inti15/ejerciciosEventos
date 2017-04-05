04b04abd0066aa8b48e57dca117ae190b4d69534
Yesenia <yeseniasescarcega@gmail.com>
2017-04-05 09:47:00 -0500

    Se agregan comentarios a los archivos html y js

--- buscaminas/buscaminas.js
+++ buscaminas/buscaminas.js
@@ -1,56 +1,55 @@
-document.addEventListener("click", clicks);
+document.addEventListener("click", clicks);//Escucha el evento click dentro del documento (al ejecutarse manda llamar la función clicks)
 
-// var pagina = document.getElementById("cuerpo");
+var tabla = document.getElementById("tabla");//Manda llamar al elemento del html que conduerde con el id
+tabla.addEventListener("click", noClicks);//Se asigna a la varia tabla un escucha de eventos (click)que ejecuta la función correspondiente
 
- var tabla = document.getElementById("tabla");
-tabla.addEventListener("click", noClicks);
-
-function clicks() {
+function clicks() {//Muestra el mensaje del alert al usuario (miestras el juego no haya terminado)
   alert(" 🎮 Sigue jugando 🎮");
 }
 
-function noClicks(){
+function noClicks(){//Detiene el flujo de eventos(ejecución de la fucnión clicks) cuando el evento sucede dentro del elemento tabla
   event.stopPropagation();
 }
 
-var i;
-function cambiaColor(boton) {
+var i; //Variable global para los ciclos for
+function cambiaColor(boton) {//Agrega el atributo style a la variable boton y cambia el color de fondo
   boton.setAttribute("style", "background-color:#607D8B;");
 }
 
-var bomba = document.getElementsByClassName("boom");
-for(i=0; i<bomba.length;i++){
-  bomba[i].addEventListener("click", mostrarBombas);
+var bomba = document.getElementsByClassName("boom");//Variable que guarda el arreglo de los elementos que concuerden con la clase correspondiente
+for(i=0; i<bomba.length;i++){//Ciclo que itera el arreglo bomba
+  bomba[i].addEventListener("click", mostrarBombas);//Se agrega un escucha de eventos (click) que ejecuta la función mostrarBombas
 }
 
-function mostrarBombas(){
+function mostrarBombas(){//Itera el arreglo bomba, modifica el valor de cada uno de los elementos
   for(i=0; i<bomba.length;i++){
     bomba[i].value = "💣";
   }
-  setTimeout(function(){alert("☠ ☠ ☠ G A M E   O V E R ☠ ☠ ☠");},500);
-  noWork();
+  setTimeout(function(){alert("☠ ☠ ☠ G A M E   O V E R ☠ ☠ ☠");},500);//Se muestra un alert con el mensaje "Game over" con un retraso de 500 milisegundos
+  noWork(); //Manda llamar la función noWork();
 }
 
 function noWork() {
-  var funcionalidad = document.getElementsByTagName("input");
-  for(i=0; i<funcionalidad.length;i++){
-    funcionalidad[i].disabled = true;
+  var funcionalidad = document.getElementsByTagName("input");//Obtiene todos los elementos del html que concuerden con la etiqueta <input>
+  for(i=0; i<funcionalidad.length;i++){//Itera el arreglo funcionalidad
+    funcionalidad[i].disabled = true; //Deshabilita la funcionalidad de los elementos del arreglo
   }
-  document.removeEventListener("click", clicks);
+  document.removeEventListener("click", clicks);//Remueve el escucha de eventos del document (con el evento y la función que ejecuta)
 }
 
-var numero = document.getElementsByClassName("numero");
-for(i=0; i<numero.length;i++){
-  numero[i].addEventListener("click", mostrarNum);
+var numero = document.getElementsByClassName("numero");//Obtiene todos los elementos del html que concuerden con la clase correspondiente
+for(i=0; i<numero.length;i++){//Itera el arreglo numero
+  numero[i].addEventListener("click", mostrarNum);//Se agrega un escucha de eventos (click) que ejecuta la función mostrarNum
 }
 
-function mostrarNum(numero) {
-  numero.toElement.value = numero.toElement.name;
+function mostrarNum(numero) {//Recibe el elemento en el que se está ejecutando el evento
+  numero.toElement.value = numero.toElement.name; //Asigna al valor del elemento el nombre del mismo elemento
 
 }
 
-var enviar = document.getElementById("enviar");
-enviar.addEventListener("click", reiniciar);
+var enviar = document.getElementById("enviar");//Obtiene el elemento del html que concuerde con el id correspondiente
+enviar.addEventListener("click", reiniciar);//Se agrega un escucha de eventos (click) que ejecuta la función reiniciar
+
 function reiniciar(){
-  location.reload();
+  location.reload();//Recarga la pagina web.
 }
